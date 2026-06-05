ffe0bb6d08aa96f91842898fab5dc452522b5aa2
JM-command <jaime00coelho@gmail.com>
2026-06-05 02:59:43 +0200

    Initial Commit

---
+++ MCTerra/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MCTerra
+//
+//  Created by Jaime Coelho on 05.06.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
